24194a71f6521211fc8ef0b1444fe5e211d2871a
Fayaz K <info@fayazk.com>
2025-01-30 15:55:14 +0500

    ♻️ (app.php): refactor admin route prefix ♻️ (app.php): make admin prefix configurable 🔧 (trail.php): add admin prefix configuration file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,23 +7,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Support\Facades\Route;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
-        then: function () {
-            Route::prefix('admin')->name('admin.')
-                ->group(base_path('routes/admin.php'));
-        },
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ]);
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+return Application::configure( basePath: dirname( __DIR__ ) )
+                  ->withRouting(
+                      web: __DIR__ . '/../routes/web.php',
+                      commands: __DIR__ . '/../routes/console.php',
+                      health: '/up',
+                      then: function() {
+                          Route::prefix( config( 'trail.admin_prefix' ) )->name( 'admin.' )
+                               ->group( base_path( 'routes/admin.php' ) );
+                      },
+                  )
+                  ->withMiddleware( function( Middleware $middleware ) {
+                      $middleware->web( append: [
+                          HandleInertiaRequests::class,
+                          AddLinkHeadersForPreloadedAssets::class,
+                      ] );
+                      //
+                  } )
+                  ->withExceptions( function( Exceptions $exceptions ) {
+                      //
+                  } )->create();
